7064f15a2240e7b5283892fc02a745bcc8dbfe84
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-21 19:31:29 +0700

    Membuat Column berisi teks

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -99,8 +99,20 @@ fun RencanaStudyView(
                         topStart = 15.dp
                     )
                 )
-                .fillMaxSize()
+                .fillMaxSize(),
         ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Pilih Mata Kuliah Peminatan", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+            }
 
         }
     }

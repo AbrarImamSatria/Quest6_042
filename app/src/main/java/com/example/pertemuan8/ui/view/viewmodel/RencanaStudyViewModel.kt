99ec1c2354dadd7954e325a5b74b104e7913aedc
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-21 20:17:58 +0700

    membuat RencanaStudyViewModel untuk mengelola krs

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.pertemuan8.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaStudyViewModel: ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUI: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
 }

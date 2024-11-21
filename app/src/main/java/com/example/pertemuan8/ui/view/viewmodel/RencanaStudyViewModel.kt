3afebe78a7e7035a76434df871a4da278d9ec78d
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-21 20:20:55 +0700

    membuat function setMataKuliah

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,9 +5,14 @@ import com.example.pertemuan8.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel: ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUI: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
 
 }

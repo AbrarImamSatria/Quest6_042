b9166cd862c681cb99f19b45a87bcc128fe608d9
abrarimamsatria09 <092004abrar@gmail.com>
2024-11-21 21:10:23 +0700

    membuat fungsi MahasiswaApp berfungsi sebagai entry point aplikasi

--- app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan8.navigation
 
+import android.widget.MediaController
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan8.ui.view.viewmodel.MahasiswaViewModel
 
 enum class Halaman {
     Splash,
@@ -9,3 +18,12 @@ enum class Halaman {
     Tampil
 }
 
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: MahasiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+
+}
